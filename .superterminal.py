226d018218575a699204ef030d9e1ec4fc686e96
Quantum-Bash <77575658+Quantum-Bash@users.noreply.github.com>
2021-07-06 17:53:17 +0500

    Add files via upload

--- .superterminal.py
+++ .superterminal.py
@@ -32,18 +32,35 @@ while True:
         exit: goes back to home.
                 """)
     if sucode == 'rsd':
-        os.system("python3.8 superterminal.py")
+        os.system("python3.8 .superterminal.py")
         print("restarting...")
         exit()
     if sucode == 'md':
         os.mkdir(input("Enter Name to create folder: "))
         print("Folder Created ")
-    if sucode == 'dr':
-        os.rmdir(input("Enter Name to delete folder: "))
-        print("Folder deleted")
+    if sucode == 'rd':
+        if os.path.exists(input("Enter Name to delete folder: ")):
+            os.rmdir(input("Enter Name of folder to confirm: "))
+            print("Folder deleted")
+        else:
+            print("The folder does not exist")
     if sucode == 'clear':
         os.system('clear')
     if sucode == 'wclear':
         os.system('cls')
     if sucode == 'exit':
         exec(open(".home.py").read())
+    if sucode == 'touch':
+        THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+        my_file = os.path.join(THIS_FOLDER, input("Please enter filename: "))
+        file = open(input("Enter filename to edit it: "), "w") 
+        file.write(input("What do you want to write to file: ")) 
+        file.close() 
+    if sucode == 'rf':
+        if os.path.exists(input("Enter file name to delete it: ")):
+            os.remove(input("Enter File name to confirm: "))
+        else:
+            print("The file does not exist")
+
+    if sucode == '':
+        pass
